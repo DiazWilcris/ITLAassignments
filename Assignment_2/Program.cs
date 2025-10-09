8f726d206a37e7a6ed221be981ccccc2621db133
DiazWilcris <wilcrisdiazv04@gmail.com>
2025-10-09 05:24:06 -0400

    Uploading Task 2

---
+++ Assignment_2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Wilcris Diaz Villanueva 20250911
+int num = 0;
+Console.WriteLine("Insert a number to check if it is even or not");
+num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+if(num % 2  == 0)
+{
+    Console.WriteLine("Your number is even");
+}
+else
+{
+    Console.WriteLine("Your number is not even");
+}
+
+Console.ReadLine();
